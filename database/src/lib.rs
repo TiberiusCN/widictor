a3b0b023862ae08bd1d7af4e8d985d9d70345dbf
tiberius <vi@ceramic3d.ru>
2020-10-22 18:59:01 +0500

    wip

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -1,4 +1,6 @@
 use rusqlite::{Connection, params};
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -10,8 +12,32 @@ pub enum Error {
   ValueNotFound(String, &'static str),
 }
 
+pub struct Word {
+  word: i64,
+  base: Base,
+}
+
+impl Word {
+  pub fn value(&self) -> Result<String, Error> {
+    self.base.word_value(self.word)
+  }
+  pub fn tags(&self) -> Result<Vec<String>, Error> {
+    self.base.word_tags(self.word)
+  }
+  pub fn forms(&self) -> Result<HashMap<String, String>, Error> {
+    self.base.word_forms(self.word)
+  }
+  pub fn insert_tag(&mut self, tag: &str) -> Result<(), Error> {
+    self.base.insert_tag(self.word, tag)
+  }
+  pub fn insert_form(&mut self, form: &str, value: &str) -> Result<(), Error> {
+    self.base.insert_form(self.word, form, value)
+  }
+}
+
+#[derive(Clone)]
 pub struct Base {
-  connection: Connection,
+  connection: Arc<RwLock<Connection>>,
 }
 
 impl Base {
@@ -20,51 +46,91 @@ impl Base {
       std::fs::File::create(path)?;
     }
     let me = Self::open(path)?;
-    me.connection.execute(
-      "CREATE TABLE words (
-        id INTEGER PRIMARY KEY,
-        word TEXT NOT NULL UNIQUE,
-        value TEXT NOT NULL
-      )",
-      params![]
-    )?;
-    me.connection.execute(
-      "CREATE TABLE tags (
-        id INTEGER PRIMARY KEY,
-        tag TEXT NOT NULL UNIQUE
-      )",
-      params![]
-    )?;
-    me.connection.execute(
-      "CREATE TABLE word_tags (
-        word INTEGER,
-        tag INTEGER,
-        PRIMARY KEY (word, tag),
-        FOREIGN KEY (word)
-          REFERENCES words (id)
-            ON DELETE CASCADE
-            ON UPDATE NO ACTION,
-        FOREIGN KEY (tag)
-          REFERENCES tags (id)
-            ON DELETE CASCADE
-            ON UPDATE NO ACTION
-      )",
-      params![]
-    )?;
-    // mutation
-    // properties
+    {
+      let connection = me.connection.write().unwrap();
+      connection.execute(
+        "CREATE TABLE words (
+          id INTEGER PRIMARY KEY,
+          word TEXT NOT NULL UNIQUE,
+          value TEXT NOT NULL
+        )",
+        params![]
+      )?;
+      connection.execute(
+        "CREATE TABLE tags (
+          id INTEGER PRIMARY KEY,
+          tag TEXT NOT NULL UNIQUE
+        )",
+        params![]
+      )?;
+      connection.execute(
+        "CREATE TABLE word_tags (
+          word INTEGER,
+          tag INTEGER,
+          PRIMARY KEY (word, tag),
+          FOREIGN KEY (word)
+            REFERENCES words (id)
+              ON DELETE CASCADE
+              ON UPDATE NO ACTION,
+          FOREIGN KEY (tag)
+            REFERENCES tags (id)
+              ON DELETE CASCADE
+              ON UPDATE NO ACTION
+        )",
+        params![]
+      )?;
+      // mutation
+      connection.execute(
+        "CREATE TABLE forms (
+          id INTEGER PRIMARY KEY,
+          form TEXT NOT NULL UNIQUE
+        )",
+        params![]
+      )?;
+      connection.execute(
+        "CREATE TABLE word_forms (
+          word INTEGER,
+          form INTEGER,
+          value TEXT NOT NULL,
+          PRIMARY KEY (word, form),
+          FOREIGN KEY (word)
+            REFERENCES words (id)
+              ON DELETE CASCADE
+              ON UPDATE NO ACTION,
+          FOREIGN KEY (form)
+            REFERENCES forms (id)
+              ON DELETE CASCADE
+              ON UPDATE NO ACTION
+        )",
+        params![]
+      )?;
+      // properties
+      connection.execute(
+        "CREATE TABLE word_properties (
+          word INTEGER,
+          property TEXT NOT NULL,
+          value TEXT NOT NULL,
+          PRIMARY KEY (word, property),
+          FOREIGN KEY (word)
+            REFERENCES words (id)
+              ON DELETE CASCADE
+              ON UPDATE NO ACTION
+        )",
+        params![]
+      )?;
+    }
     Ok(me)
   }
 
   pub fn open(path: &std::path::Path) -> Result<Self, Error> {
-    let connection = Connection::open(path)?;
+    let connection = Arc::new(RwLock::new(Connection::open(path)?));
     Ok(Self {
       connection
     })
   }
 
-  pub fn insert_word(&mut self, word: &str, value: &str) -> Result<i64, Error> {
-    self.connection.execute(
+  pub fn insert_word(&mut self, word: &str, value: &str) -> Result<Word, Error> {
+    self.connection.write().unwrap().execute(
       "INSERT INTO words (word, value) VALUES (?1, ?2)",
       params![
         word,
@@ -75,7 +141,7 @@ impl Base {
   }
 
   fn new_tag(&mut self, tag: &str) -> Result<i64, Error> {
-    self.connection.execute(
+    self.connection.write().unwrap().execute(
       "INSERT INTO tags (tag) VALUES (?1)",
       params![
         tag,
@@ -84,29 +150,83 @@ impl Base {
     self.search_tag(tag)
   }
 
-  pub fn search_word(&self, word: &str) -> Result<i64, Error> {
-    let mut stmt = self.connection.prepare("SELECT id FROM words WHERE word = ?1")?;
-    let mut iter = stmt.query_map(params![word], |row| -> Result<i64, rusqlite::Error> {
-      row.get(0)
-    })?;
-    Ok(iter.next().ok_or_else(|| Error::ValueNotFound(word.to_owned(), "word"))??)
+  fn new_form(&mut self, form: &str) -> Result<i64, Error> {
+    self.connection.write().unwrap().execute(
+      "INSERT INTO forms (form) VALUES (?1)",
+      params![
+        form,
+      ]
+    )?;
+    self.search_form(form)
+  }
+
+  pub fn search_word(&self, word: &str) -> Result<Word, Error> {
+    let word = {
+      let connection = self.connection.read().unwrap();
+      let mut stmt = connection.prepare("SELECT id FROM words WHERE word = ?1")?;
+      let mut iter = stmt.query_map(params![word], |row| -> Result<i64, rusqlite::Error> {
+        row.get(0)
+      })?;
+      iter.next().ok_or_else(|| Error::ValueNotFound(word.to_owned(), "word"))??
+    };
+    Ok(Word {
+      word,
+      base: self.clone(),
+    })
+  }
+
+  pub fn search_word_or_form(&self, word: &str) -> Result<Word, Error> {
+    match self.search_word(word) {
+      Ok(s) => return Ok(s),
+      Err(Error::ValueNotFound(..)) => {},
+      Err(e) => return Err(e),
+    };
+    self.search_form_of_word(word)
+  }
+
+  pub fn search_form_of_word(&self, word: &str) -> Result<Word, Error> {
+    let word = {
+      let connection = self.connection.read().unwrap();
+      let mut stmt = connection.prepare(
+        "SELECT id FROM words
+        INNER JOIN word_forms ON word_forms.word = words.word
+        WHERE word_forms.value = ?1")?;
+      let mut iter = stmt.query_map(params![word], |row| -> Result<i64, rusqlite::Error> {
+        row.get(0)
+      })?;
+      iter.next().ok_or_else(|| Error::ValueNotFound(word.to_owned(), "word form"))??
+    };
+    Ok(Word {
+      word,
+      base: self.clone(),
+    })
   }
 
   fn search_tag(&self, tag: &str) -> Result<i64, Error> {
-    let mut stmt = self.connection.prepare("SELECT id FROM tags WHERE tag = ?1")?;
+    let connection = self.connection.read().unwrap();
+    let mut stmt = connection.prepare("SELECT id FROM tags WHERE tag = ?1")?;
     let mut iter = stmt.query_map(params![tag], |row| -> Result<i64, rusqlite::Error> {
       row.get(0)
     })?;
     Ok(iter.next().ok_or_else(|| Error::ValueNotFound(tag.to_owned(), "tag"))??)
   }
 
-  pub fn insert_tag(&mut self, word: i64, tag: &str) -> Result<(), Error> {
+  fn search_form(&self, form: &str) -> Result<i64, Error> {
+    let connection = self.connection.read().unwrap();
+    let mut stmt = connection.prepare("SELECT id FROM forms WHERE form = ?1")?;
+    let mut iter = stmt.query_map(params![form], |row| -> Result<i64, rusqlite::Error> {
+      row.get(0)
+    })?;
+    Ok(iter.next().ok_or_else(|| Error::ValueNotFound(form.to_owned(), "form"))??)
+  }
+
+  fn insert_tag(&mut self, word: i64, tag: &str) -> Result<(), Error> {
     let tag = match self.search_tag(tag) {
       Ok(id) => id,
       Err(Error::ValueNotFound(..)) => self.new_tag(tag)?,
       Err(e) => return Err(e),
     };
-    self.connection.execute(
+    self.connection.write().unwrap().execute(
       "INSERT INTO word_tags (word, tag) VALUES (?1, ?2)",
       params![
         word,
@@ -116,8 +236,26 @@ impl Base {
     Ok(())
   }
 
-  pub fn word_tags(&mut self, word: i64) -> Result<Vec<String>, Error> {
-    let mut stmt = self.connection.prepare(
+  fn insert_form(&mut self, word: i64, form: &str, value: &str) -> Result<(), Error> {
+    let form = match self.search_form(form) {
+      Ok(id) => id,
+      Err(Error::ValueNotFound(..)) => self.new_form(form)?,
+      Err(e) => return Err(e),
+    };
+    self.connection.write().unwrap().execute(
+      "INSERT INTO word_forms (word, form, value) VALUES (?1, ?2, ?3)",
+      params![
+        word,
+        form,
+        value,
+      ]
+    )?;
+    Ok(())
+  }
+
+  fn word_tags(&self, word: i64) -> Result<Vec<String>, Error> {
+    let connection = self.connection.read().unwrap();
+    let mut stmt = connection.prepare(
       "SELECT tags.tag FROM tags
         INNER JOIN word_tags ON word_tags.tag = tags.id
         WHERE word_tags.word = ?1")?;
@@ -127,8 +265,21 @@ impl Base {
     Ok(iter.collect::<Result<Vec<_>, _>>()?)
   }
 
-  pub fn word_value(&self, word: i64) -> Result<String, Error> {
-    let mut stmt = self.connection.prepare("SELECT value FROM words WHERE id = ?1")?;
+  fn word_forms(&self, word: i64) -> Result<HashMap<String, String>, Error> {
+    let connection = self.connection.read().unwrap();
+    let mut stmt = connection.prepare(
+      "SELECT forms.form, word_forms.value FROM forms
+        INNER JOIN word_forms ON word_forms.form = forms.id
+        WHERE word_forms.word = ?1")?;
+    let iter = stmt.query_map(params![word], |row| -> Result<(String, String), rusqlite::Error> {
+      Ok((row.get(0)?, row.get(1)?))
+    })?;
+    Ok(iter.collect::<Result<HashMap<String, String>, _>>()?)
+  }
+
+  fn word_value(&self, word: i64) -> Result<String, Error> {
+    let connection = self.connection.read().unwrap();
+    let mut stmt = connection.prepare("SELECT value FROM words WHERE id = ?1")?;
     let mut iter = stmt.query_map(params![word], |row| -> Result<String, rusqlite::Error> {
       row.get(0)
     })?;
@@ -144,10 +295,15 @@ mod test {
   fn table() {
     let mut base = Base::create(&std::path::Path::new("/tmp/lang.db")).unwrap();
     base.insert_word("word", "value").unwrap();
-    let word = base.insert_word("wörd", "translate").unwrap();
-    base.insert_tag(word, "noun").unwrap();
-    base.insert_tag(word, "neuter").unwrap();
-    println!("{:?}", base.word_value(word).unwrap());
-    println!("{:?}", base.word_tags(word).unwrap());
+    let mut word = base.insert_word("wörd", "translate").unwrap();
+    word.insert_tag("noun").unwrap();
+    word.insert_tag("neuter").unwrap();
+    word.insert_form("AccSg", "wordem").unwrap();
+    word.insert_form("VocSg", "wordi").unwrap();
+    println!("{:?}", word.value().unwrap());
+    println!("{:?}", word.tags().unwrap());
+    println!("{:?}", word.forms().unwrap());
+    println!("{:?}", base.search_word_or_form("word").unwrap().value().unwrap());
+    println!("{:?}", base.search_word_or_form("wordem").unwrap().value().unwrap());
   }
 }
