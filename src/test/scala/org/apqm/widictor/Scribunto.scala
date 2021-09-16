df5436ca591d14e9b2150cf1d3c5a4dad7b4ba63
tiberiuscn <tican@protonmail.com>
2021-09-16 23:48:09 +0500

    tests!

--- src/test/scala/org/apqm/widictor/Scribunto.scala
+++ src/test/scala/org/apqm/widictor/Scribunto.scala
@@ -1,17 +1,18 @@
 package org.apqm.widictor
 
-import utest._
+import org.scalatest._
 import scala.util._
 import org.parboiled2._
 
-object ScribuntoAnswerSpec extends TestSuite {
-  val tests = Tests {
-    "The ScribuntoAnswer" - {
-      "parse 'N' to None" - {
-        parse("N") ==> None
+object ScribuntoAnswerSpec extends WordSpec {
+  "The ScribuntoAnswer" when {
+    "N" should {
+      "produce None" in {
+        assert(parse("N"), None)
       }
     }
   }
+
   def parse(s: String): Object = {
     val parser = new ScribuntoAnswer(s)
     parser.Lua.run()
