e2f6b997fb221fd55ea616b3e2c7699032e63725
tiberiuscn <tican@protonmail.com>
2021-11-17 01:17:56 +0500

    wip

--- src/main/scala/org/apqm/widictor/App.scala
+++ src/main/scala/org/apqm/widictor/App.scala
@@ -4,20 +4,9 @@ import org.parboiled2._
 import scala.io.AnsiColor._
 
 object Main extends App {
-  val onError = { parser: WikiParser => { z: Throwable => z match {
-    case e: ParseError => RED + parser.formatError(e) + RESET
-    case n => n.toString
-  }}}
-  val p = new WikiParser(java.nio.file.Files.readString(java.nio.file.Paths.get("test")), "French")
-  val out = p.total.run().toEither.left.map(onError(p))
+  val out = WikiParser.run(java.nio.file.Files.readString(java.nio.file.Paths.get("test")), "French")
   out match {
     case Left(e) => println(s"${RED}$e${RESET}")
-    case Right(s) => s.reverse.map { l =>
-      var p = ""
-      l.reverse.map { r =>
-        println(p + r)
-        p = p + "-"
-      }
-    }
+    case Right(s) => println(s)
   }
 }
--- src/main/scala/org/apqm/widictor/Scribunto.scala
+++ src/main/scala/org/apqm/widictor/Scribunto.scala
@@ -4,13 +4,21 @@ import org.parboiled2._
 import cats.implicits._
 import shapeless.HNil
 
-sealed trait WikiAst
-case class Language(lang: String) extends WikiAst
-case class Section(name: String, level: Int) extends WikiAst
-case class RawText(text: String) extends WikiAst
-case class Template(rule: Seq[List[WikiAst]], params: Seq[List[WikiAst]]) extends WikiAst
-case object NewLine extends WikiAst
-case object HalfLine extends WikiAst
+sealed trait WikiAst[A]
+case class Language[A](lang: String, var sections: List[Section[A]], var text: A) extends WikiAst[A] {
+  def map[B](f: A => B) = Language[B](lang, sections.map(_.map(f)), f(text))
+}
+case class Section[A](name: String, level: Int, var subsections: List[Section[A]], var text: A) extends WikiAst[A] {
+  def map[B](f: A => B): Section[B] = Section[B](name, level, subsections.map(_.map(f)), f(text))
+}
+case object NewLine extends WikiAst[RawText]
+case object LineSpace extends WikiAst[RawText]
+sealed trait WikiTextAst
+case class RawText(var text: String) extends WikiAst[RawText] with WikiTextAst
+case class WikiText(text: List[WikiTextAst]) extends WikiAst[WikiTextAst] with WikiTextAst
+case class WikiTemplate(main: String, params: Map[String, WikiTextAst]) extends WikiTextAst
+
+// case class Template(rule: Seq[List[WikiAst]], params: Seq[List[WikiAst]]) extends WikiAst
 
 class WikiParser(val input: ParserInput, val langFilter: String) extends Parser {
   def nl = rule { '\n' }
@@ -19,71 +27,91 @@ class WikiParser(val input: ParserInput, val langFilter: String) extends Parser 
   def sectionMark = rule { "==" ~ capture(mark.*) ~> (_.length) }
   def section = rule {
     sectionMark ~ capture(printable) ~ sectionMark ~> { (b, p, e) =>
-      test(b == e) ~ push(if (e == 0) Language(p) else Section(p, e - 1))
+      test(b == e) ~ push(if (e == 0) Language(p, List(), RawText("")) else Section(p, e - 1, List(), RawText("")))
     }
   }
-  def rawText = rule { capture(noneOf("\n").+) ~> (RawText(_)) }
-  def template = rule { openTemplate ~ closeTemplate ~ push(RawText("TEMPLATE")) }
-  def empty: Rule1[WikiAst] = rule { push(HalfLine) }
-  def emptyLine: Rule1[WikiAst] = rule { '\n' ~ &('\n') ~ push(NewLine) }
-  def line: Rule1[WikiAst] = rule { section | rawText | emptyLine | empty }
-  def total: Rule1[List[List[WikiAst]]] = rule { line.+(nl) ~ nl.? ~> { seq =>
+  def rawText = rule { capture(noneOf("\n").+) ~> (j => RawText(j)) }
+  // def template = rule { openTemplate ~ closeTemplate ~ push(RawText("TEMPLATE")) }
+  def empty = rule { push(LineSpace) }
+  def emptyLine = rule { '\n' ~ &('\n') ~ push(NewLine) }
+  def line = rule { section | rawText | emptyLine | empty }
+  def total: Rule1[Option[Language[RawText]]] = rule { line.+(nl) ~ nl.? ~> { seq =>
     var filter = false
     seq.toList.flatMap {
-      case node @ Language(lang) =>
-        filter = lang == langFilter
-        None
+      case node: Language[_] =>
+        filter = node.lang == langFilter
+        if (filter) Some(node) else None
       case node => if (filter) Some(node) else None
-    }.foldLeft(List[List[WikiAst]]())(mergeTree)
+    }.foldLeft(List[WikiAst[RawText]]())(insert).lastOption.flatMap {
+      case l: Language[_] => Some(l)
+      case _ => None
+    }
   } ~ EOI }
 
   def openTemplate = rule { "{{" }
   def closeTemplate = rule { "}}" }
   def unicodePrefix = rule { "\\u" }
-  def quote = rule { "\\\"" }
   def pureText = rule { capture(noneOf("\n{}|").+) ~> (RawText(_)) }
-  // def template = rule { openTemplate ~ pureText.+.separatedBy('|') ~ closeTemplate ~> {
-  //   case Seq() => test(false)
-  //   case Seq(j) => Template(j, Seq())
-  //     ???
-  // }}
-  def textElement = rule { pureText | template }
-  def text: Rule1[Seq[WikiAst]] = rule { textElement.* }
+  def templateParamsRaw: Rule1[Seq[Seq[WikiTextAst]]] = rule { { '|' ~ textRaw }.* }
+  def templateParams: Rule1[Map[String, WikiTextAst]] = rule { templateParamsRaw ~> { params: Seq[Seq[WikiTextAst]] =>
+    var id = 0
+    params.map { seq =>
+      id += 1
+      id.toString -> WikiText(seq.toList)
+    }.toMap
+  }}
+  def template = rule { openTemplate ~ pureText ~ templateParams ~ closeTemplate ~> { (name, params) =>
+    WikiTemplate(name.text, params)
+  }}
+  def textElement = rule { template | pureText }
+  def textRaw: Rule1[Seq[WikiTextAst]] = rule { textElement.* }
+  def text: Rule1[Seq[WikiTextAst]] = rule { textRaw ~ EOI }
 
-  private def mergeTree(acc: List[List[WikiAst]], line: WikiAst): List[List[WikiAst]] = {
-    val (current, branches) = acc match {
-      case Nil => (List(), List())
-      case a :: tail => (a, tail)
-    }
-    import scala.io.AnsiColor._
-    merge(current, line, None) ++ branches
-  }
-  private def merge(current: List[WikiAst], line: WikiAst, root: Option[List[WikiAst]]): List[List[WikiAst]] = {
-    val ret: List[WikiAst] => List[List[WikiAst]] = { next =>
-      root match {
-        case Some(root) => next :: root :: Nil
-        case None => next :: Nil
+  @scala.annotation.tailrec
+  private def insert(acc: List[WikiAst[RawText]], line: WikiAst[RawText]): List[WikiAst[RawText]] = {
+    (acc, line) match {
+      case (Nil, line: Language[_]) => line :: acc
+      case (Nil, _) => Nil
+      case (up :: tail, line) => (up, line) match {
+        case (up: RawText, RawText(down)) =>
+          up.text += down
+          up :: tail
+        case (up: RawText, NewLine) =>
+          up.text += '\n'
+          up :: tail
+        case (l: Language[_], s: Section[_]) =>
+          l.sections = s :: l.sections
+          s :: l :: tail
+        case (l: Language[_], RawText(t)) =>
+          l.text.text += t
+          l.text :: l :: tail
+        case (s: Section[_], RawText(t)) =>
+          s.text.text += t
+          s.text :: s :: tail
+        case (_: Language[_], NewLine)
+            | (_: Section[_], NewLine)
+            | (_, LineSpace)
+            => up :: tail
+        case (up: Section[_], down: Section[_]) if (up.level < down.level) =>
+          up.subsections = down :: up.subsections
+          down :: up :: tail
+        case _ => insert(tail, line)
       }
     }
-    (current, line) match {
-      case (Nil, j) => ret(j :: Nil)
-      case (up :: tail, down) =>
-        (up, down) match {
-          case (RawText(up), RawText(down)) => ret(RawText(up + down) :: tail)
-          case (_: Language, _: Section)
-             | (_: Language, _: RawText)
-             | (_: Section, _: RawText)
-             => ret(down :: up :: tail)
-          case (_: Language, NewLine)
-             | (_: Section, NewLine)
-             | (_: RawText, NewLine)
-             => ret(up :: tail)
-          case (Section(_, levelu), Section(_, leveld)) if (levelu < leveld) =>
-            ret(down :: up :: tail)
-          case (RawText(up), _) =>
-            merge(tail, down, Some(RawText(up) :: tail))
-          case _ => merge(tail, down, Some(root.getOrElse(current)))
-        }
-    }
+  }
+}
+object WikiParser {
+  def run(source: String, lang: String) = {
+    val onError = { parser: WikiParser => { z: Throwable => z match {
+      case e: ParseError => parser.formatError(e)
+      case n => n.toString
+    }}}
+    val p = new WikiParser(source, lang)
+    p.total.run().toEither.left.map(onError(p)).map(_.map(_.map { j =>
+      val p = new WikiParser(j.text, lang)
+      val z = p.text.run().toEither.left.map(onError(p))
+      import scala.io.AnsiColor._
+      println(s"$RED$j$RESET → $z")
+    }))
   }
 }
